1143a17445b8f0ffbb4b828ac328f03fd657b692
wellingtonacosta <wellingtonacosta09@gmail.com>
2023-04-20 11:32:30 -0300

    first commit

---
+++ SquadManager/Models/UserViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace SquadManager.Models
+{
+	public class UserViewModel
+	{
+		
+		
+		public string Username { get; set; }
+
+		public string Email { get; set; }
+
+		public string Password { get; set; }
+	}
+}
+
+
