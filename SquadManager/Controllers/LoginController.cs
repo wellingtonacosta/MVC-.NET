c5d63c98deae50dad3ed2b2bae3012ef41ffdf30
wellingtonacosta <wellingtonacosta09@gmail.com>
2023-04-21 18:18:43 -0300

    add post method

--- SquadManager/Controllers/LoginController.cs
+++ SquadManager/Controllers/LoginController.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SquadManager.Models;
 
+
+// TODO: make the interface
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace SquadManager.Controllers
@@ -13,8 +16,20 @@ namespace SquadManager.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            UserViewModel user = new UserViewModel();
+            user.Email = "email enviado";
+            return View("Index", user);
+
         }
+
+        [HttpPost]
+
+        public IActionResult Test(UserViewModel user)
+        {
+           user.Email = "email enviado";
+           return View("Index", user);
+        }
+
     }
 }
 
